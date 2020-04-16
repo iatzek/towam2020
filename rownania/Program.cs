900ae4dacb4507ed694cbe90a1beff783fe992ef
Jacek Kawa <jacek.kawa@polsl.pl>
2020-04-16 12:42:09 +0200

    zmiana komunikatu

--- rownania/Program.cs
+++ rownania/Program.cs
@@ -6,7 +6,7 @@ namespace rownania
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
