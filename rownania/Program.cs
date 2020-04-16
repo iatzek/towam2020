a71f847ba4d1d9c36bc249406090aeec29cdc553
Jacek Kawa <jacek.kawa@polsl.pl>
2020-04-16 12:46:46 +0200

    pierwsza wersja

--- rownania/Program.cs
+++ rownania/Program.cs
@@ -6,7 +6,16 @@ namespace rownania
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            double a = 1, b = 0, c = -25;
+            double delta = Math.Sqrt(b * b - 4 * a * c);
+
+            double x1 = (-b - delta) / 2 * a;
+            double x2 = (-b + delta) / 2 * a;
+
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+
+
         }
     }
 }
