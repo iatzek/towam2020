0bef527e790c29fd95ffb7f488cfff25887e67db
Jacek Kawa <jacek.kawa@polsl.pl>
2020-04-16 13:00:09 +0200

    fix: 2a

--- rownania/Program.cs
+++ rownania/Program.cs
@@ -9,8 +9,8 @@ namespace rownania
             double a = 1, b = 0, c = -36;
             double delta = Math.Sqrt(b * b - 4 * a * c);
 
-            double x1 = (-b - delta) / 2 * a;
-            double x2 = (-b + delta) / 2 * a;
+            double x1 = (-b - delta) / (2 * a);
+            double x2 = (-b + delta) / (2 * a);
 
             Console.WriteLine(x1);
             Console.WriteLine(x2);
